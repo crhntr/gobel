8d59bc62982e158a50dfd3aa8b400040e0b8d383
crhntr <hunter.christopher@icloud.com>
2017-01-29 18:14:51 -0800

    added comment to Equals method

--- es6/es6lexer/token.go
+++ es6/es6lexer/token.go
@@ -80,6 +80,7 @@ func (typ Type) String() string {
 	}
 }
 
+// Equals checks if the token is equal to another token
 func (tok Token) Equals(tok2 Token) bool {
 	return tok.Type == tok2.Type && tok.Value == tok2.Value
 }
