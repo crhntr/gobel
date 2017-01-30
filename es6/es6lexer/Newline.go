7d1239a56d3f5757d4c4a0fb59bf63a9dee0e51d
crhntr <hunter.christopher@icloud.com>
2017-01-30 15:25:39 -0800

    added isLineTerminator helper function

--- es6/es6lexer/Newline.go
+++ es6/es6lexer/Newline.go
@@ -1,5 +1,8 @@
 package es6lexer
 
+func isLineTerminator(r rune) bool {
+	return r == '\u000A' || r == '\u000D' || r == '\u2028' || r == '\u2029'
+}
 func hasLineTerminatorPrefix(l *Lexer) bool {
 	defer l.reset()
 	return l.accept("\u000A\u000D\u2028\u2029")
