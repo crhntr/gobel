c590ae166d65dde822e783c5ba15ba4d4c59d373
crhntr <hunter.christopher@icloud.com>
2017-01-29 19:59:20 -0800

    Added EscapeSequence function

--- es6/es6lexer/EscapeSequence.go
+++ es6/es6lexer/EscapeSequence.go
@@ -1,34 +1,28 @@
 package es6lexer
 
 // // EscapeSequence :: CharacterEscapeSequence || 0 [lookahead ∉ DecimalDigit] || HexEscapeSequence || UnicodeEscapeSequence
-// func lexEscapeSequence(l *lexer) {
+// func lexEscapeSequence(l *Lexer) {
 // 	// CharacterEscapeSequence
+// 	if l.accept("\\") {
+// 		// SingleEscapeCharacter :: ' " \ b f n r t v
+// 		if l.accept("'\"\\bfnrtv") {
+// 			return
+// 		}
 //
-// 	// SingleEscapeCharacter :: ' " \ b f n r t v
-// 	if l.accept("'\"\\bfnrtv") {
-// 		return
-// 	}
-// 	l.reset()
+// 		// 0 [lookahead ∉ DecimalDigit]
+// 		if l.accept("0") && strings.ContainsRune(decimalDigits, l.peek()) {
+// 			return
+// 		}
 //
-// 	// 0 [lookahead ∉ DecimalDigit]
-// 	if l.accept("0") && !l.accept(decimalDigits) {
-// 		return
-// 	}
+// 		// HexEscapeSequence :: x HexDigit HexDigit
+// 		if l.accept("x") && l.acceptRunN(decimalDigits+"abcdefABCDEF", 2) {
+// 			return
+// 		}
 //
-// 	// HexEscapeSequence :: x HexDigit HexDigit
-// 	if l.accept("x") && l.acceptRunN(decimalDigits[:8], 2) {
-// 		return
+// 		// UnicodeEscapeSequence :: u Hex4Digits
+// 		if l.accept("u") &&
+// 			(l.acceptRunN(decimalDigits+"abcdefABCDEF", 4) || l.accept("{") && l.acceptRunN(decimalDigits+"abcdefABCDEF", 4) && l.accept("}")) {
+// 			return
+// 		}
 // 	}
-//
-// 	// UnicodeEscapeSequence :: u Hex4Digits
-// 	if l.accept("u") && l.acceptRunN(decimalDigits[:8], 4) {
-// 		return
-// 	}
-// 	l.reset()
-//
-// 	// UnicodeEscapeSequence :: u{ HexDigits }
-// 	if l.accept("u") && l.accept("{") && l.acceptRunN(decimalDigits[:8], 4) && l.accept("}") {
-// 		return
-// 	}
-// 	l.reset()
 // }
