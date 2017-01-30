69c2293011b20cf383131eced54d49cd20c941d5
crhntr <hunter.christopher@icloud.com>
2017-01-30 10:02:02 -0800

    removed punctuator

--- es6/es6lexer/InputElement.go
+++ es6/es6lexer/InputElement.go
@@ -1,6 +1,9 @@
 package es6lexer
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // lexMux multiplexes the various states based on
 // input prefix checks. it follows the following rules from
@@ -11,6 +14,7 @@ import "strings"
 // InputElementRegExpOrTemplateTail :: 	WhiteSpace | LineTerminator | Comment | CommonToken | 																				RegularExpressionLiteral | 	TemplateSubstitutionTail
 // InputElementTemplateTail ::  				WhiteSpace | LineTerminator | Comment | CommonToken | DivPunctuator | 																										TemplateSubstitutionTail
 func lexMux(l *Lexer) stateFunc {
+	fmt.Println(l.goal)
 	switch {
 	case hasWhiteSpacePrefix(l):
 		return lexWhiteSpace
@@ -34,54 +38,16 @@ func lexMux(l *Lexer) stateFunc {
 		return lexStringLiteralDouble(l)
 	case strings.HasPrefix(l.input[l.pos:], "'"): // StringLiteral
 		return lexStringLiteralSingle(l)
-	case (l.goal == InputElementDiv || l.goal == InputElementTemplateTail) && hasDivPunctuator(l): // DivPunctuator
+	case (l.goal == InputElementRegExpOrTemplateTail || l.goal == InputElementTemplateTail) &&
+		strings.HasPrefix(l.input[l.pos:], "}"): // TemplateSubstitutionTail
+		return lexTemplateSubstitutionTail(l)
+	case (l.goal == InputElementDiv || l.goal == InputElementTemplateTail) &&
+		hasDivPunctuator(l): // DivPunctuator
 		return lexDivPunctuator(l)
-	case (l.goal == InputElementDiv || l.goal == InputElementRegExp) && strings.HasPrefix(l.input[l.pos:], "}"): // RightBracePunctuator
+	case (l.goal == InputElementDiv || l.goal == InputElementRegExp) &&
+		!(l.goal == InputElementRegExpOrTemplateTail || l.goal == InputElementTemplateTail) &&
+		strings.HasPrefix(l.input[l.pos:], "}"): // RightBracePunctuator
 		return lexRightBracePunctuator(l)
 	}
 	return nil
-}
-
-// func f(l *lexer) (stateFunc, bool) {
-// 	return nil, false
-// }
-
-var punctuators = []string{
-	"{", "(", ")", ";", "]", "[", ",",
-	">>>=", "<<=", "!==", "===", ">>>", "...", ".", ">>=", ">=",
-	"%=", "*=", "-=", "<=", "&=", "==", "!=", "|=",
-	"^=", "+=", "<<", "||", "&&", "++", "--", "=>",
-	">>", "-", "&", "|", "^", "!", "~", "%",
-	"*", "?", ":", "=", "+", ">", "<"}
-
-func hasPunctuator(l *Lexer) bool {
-	defer l.reset()
-	return l.acceptAnyString(punctuators)
-}
-
-func lexPunctuator(l *Lexer) stateFunc {
-	l.acceptAnyString(punctuators)
-	l.emit(Punctuator)
-	return lexMux
-}
-
-func hasDivPunctuator(l *Lexer) bool {
-	defer l.reset()
-	return l.acceptAnyString([]string{"/=", "/"})
-}
-
-func lexDivPunctuator(l *Lexer) stateFunc {
-	l.acceptAnyString([]string{"/=", "/"})
-	l.emit(DivPunctuator)
-	return lexMux
-	// }
-}
-
-func lexRightBracePunctuator(l *Lexer) stateFunc {
-	// if strings.HasPrefix(l.input[l.pos:], "}") {
-	l.accept("}")
-	l.emit(RightBracePunctuator)
-	return lexMux
-	// }
-	// return l.error(fmt.Errorf("div punctuator not found")) // Paranoic (should never happen)
 }
