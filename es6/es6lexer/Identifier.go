3ed7c65b064405e4b39ab81934ea3ef844585e91
crhntr <hunter.christopher@icloud.com>
2017-01-30 15:26:13 -0800

    added helper functions sIdentifierStart and isIdentifierPart

--- es6/es6lexer/Identifier.go
+++ es6/es6lexer/Identifier.go
@@ -2,16 +2,21 @@ package es6lexer
 
 import "unicode"
 
-func hasIdentifierNameStartPrefix(l *Lexer) bool {
-	r := l.peek()
+func isIdentifierStart(r rune) bool {
 	return r == '$' || r == '_' ||
 		(unicode.IsOneOf([]*unicode.RangeTable{unicode.L, unicode.Nd, unicode.Other_ID_Start}, r) &&
 			!(unicode.IsOneOf([]*unicode.RangeTable{unicode.Pattern_Syntax, unicode.Pattern_White_Space}, r)))
 }
 
+func isIdentifierPart(r rune) bool {
+	return r == '$' || r == '_' || isIdentifierStart(r) || unicode.IsOneOf([]*unicode.RangeTable{unicode.L, unicode.Mn, unicode.Mc, unicode.Nd, unicode.Pc, unicode.Other_ID_Start}, r)
+}
+func hasIdentifierNameStartPrefix(l *Lexer) bool {
+	return isIdentifierStart(l.peek())
+}
+
 func hasIdentifierNameContinuePrefix(l *Lexer) bool {
-	r := l.peek()
-	return hasIdentifierNameStartPrefix(l) || unicode.IsOneOf([]*unicode.RangeTable{unicode.L, unicode.Mn, unicode.Mc, unicode.Nd, unicode.Pc, unicode.Other_ID_Start}, r)
+	return isIdentifierPart(l.peek())
 }
 
 func lexIdentifierName(l *Lexer) stateFunc {
