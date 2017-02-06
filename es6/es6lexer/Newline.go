6dbd9f7fab3f28c4212db53339d34c4a2d13fc8e
crhntr <hunter.christopher@icloud.com>
2017-02-06 15:46:46 -0800

    Refactored hasLineTerminatorPrefix and remved isLineTerminator

--- es6/es6lexer/Newline.go
+++ es6/es6lexer/Newline.go
@@ -1,12 +1,7 @@
 package es6lexer
 
-import "strings"
-
 var lineTerminators = "\u000A\u000D\u2028\u2029"
 
-func isLineTerminator(r rune) bool {
-	return strings.ContainsRune(lineTerminators, r)
-}
 func hasLineTerminatorPrefix(l *Lexer) bool {
 	defer l.reset()
 	return l.accept(lineTerminators)
