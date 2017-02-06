54a2b62036131f41f9ae30c1634172af843a46d5
crhntr <hunter.christopher@icloud.com>
2017-02-06 15:34:16 -0800

    Refactor RegEx lexer to conform to Spec.

--- es6/es6lexer/Regex.go
+++ es6/es6lexer/Regex.go
@@ -2,31 +2,21 @@ package es6lexer
 
 func lexRegex(l *Lexer) stateFunc {
 	l.accept("/")
-	var r rune
-	for r = l.next(); ; r = l.next() {
-		if r == eof {
-			l.errorf("regex did not close with '/' ")
-			return nil
-		}
-		if isLineTerminator(r) {
-			l.errorf("regex can't have new lines")
-			return nil
-		}
-		if l.accept("/") {
-			break
-		}
-		if !isIdentifierPart(r) {
-			l.errorf("invalid character '%c' in regex", r)
-			return nil
-		}
+
+	l.acceptSourceCharacterRunExcept(lineTerminators + "/")
+	if !l.accept("/") {
+		l.errorf("regex did not close with '/' ")
+		return nil
 	}
 
 	for {
-		r = l.next()
-		if r == eof || !isIdentifierPart(r) {
+		r := l.next()
+		if !isIdentifierPart(r) {
+			l.backup()
 			break
 		}
 	}
+
 	l.emit(RegEx)
 
 	return nil
