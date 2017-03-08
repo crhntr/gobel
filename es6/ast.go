beae6232b5c25e4feed7d7ab2fe724220512f6fd
crhntr <hunter.christopher@icloud.com>
2017-03-07 20:41:04 -0800

    Stubs for A.4 Functions and Classes

--- es6/ast.go
+++ es6/ast.go
@@ -5,6 +5,7 @@ type Positioner interface {
   Position() (filename string, offset int, line int, column int)
 }
 
+// ASTNode ...
 type ASTNode interface {
   Parse(l *Lexer) (ASTNode, error)
 }
@@ -17,6 +18,188 @@ type node struct {
 func (n *node) Position() (filename string, offset int, line int, column int) {
   return n.filename, n.offset, n.line, n.column
 }
+
+//
+// A.4 Functions and Classes
+//
+
+// FunctionDeclarationNode [Yield, Default] :
+//  function BindingIdentifier[?Yield] ( FormalParameters ) { FunctionBody }
+//  [+Default] function ( FormalParameters ) { FunctionBody }
+type FunctionDeclarationNode struct {
+  node
+}
+
+// FunctionExpressionNode  : [See 14.1]
+//  function BindingIdentifieropt ( FormalParameters ) { FunctionBody }
+type FunctionExpressionNode struct {
+  node
+}
+// StrictFormalParametersNode [Yield] : [See 14.1]
+//  FormalParameters[?Yield]
+type StrictFormalParametersNode struct {
+  node
+}
+// FormalParametersNode [Yield] : [See 14.1]
+//  [empty]
+//  FormalParameterList[?Yield]
+type FormalParametersNode struct {
+  node
+}
+
+// FormalParameterListNode [Yield] : [See 14.1]
+//  FunctionRestParameter[?Yield]
+//  FormalsList[?Yield]
+//  FormalsList[?Yield] , FunctionRestParameter[?Yield]
+type FormalParameterListNode struct {
+  node
+}
+// FormalsListNode [Yield] : [See 14.1]
+//  FormalParameter[?Yield]
+//  FormalsList[?Yield] , FormalParameter[?Yield]
+type FormalsListNode struct {
+  node
+}
+
+// FunctionRestParameterNode [Yield] : [See 14.1]
+//  BindingRestElement[?Yield]
+type FunctionRestParameterNode struct {
+  node
+}
+
+// FormalParameterNode [Yield] : [See 14.1]
+//  BindingElement[?Yield]
+type FormalParameterNode struct {
+  node
+}
+// FunctionBodyNode [Yield] : [See 14.1]
+//  FunctionStatementList[?Yield]
+type FunctionBodyNode struct {
+  node
+}
+// FunctionStatementListNode [Yield] : [See 14.1]
+//  StatementList[?Yield, Return]opt
+type FunctionStatementListNode struct {
+  node
+}
+// ArrowFunctionNode [In, Yield] : [See 14.2]
+//  ArrowParameters[?Yield] [no LineTerminator here] => ConciseBody[?In]
+type ArrowFunctionNode struct {
+  node
+}
+
+// ArrowParametersNode [Yield] : [See 14.2]
+//  BindingIdentifier[?Yield]
+//  CoverParenthesizedExpressionAndArrowParameterList[?Yield]
+// ArrowParameters[Yield] : CoverParenthesizedExpressionAndArrowParameterList[?Yield]
+// is recognized the following grammar is used to refine the interpretation of CoverParenthesizedExpressionAndArrowParameterList :
+type ArrowParametersNode struct {
+  node
+}
+
+// ConciseBodyNode [In] : [See 14.2]
+//  [lookahead ≠ { ] AssignmentExpression[?In]
+//  { FunctionBody }
+//  ArrowFormalParameters[Yield] :
+//  ( StrictFormalParameters[?Yield] )
+type ConciseBodyNode struct {
+  node
+}
+
+// MethodDefinitionNode [Yield] : [See 14.3]
+//  PropertyName[?Yield] ( StrictFormalParameters ) { FunctionBody }
+//  GeneratorMethod[?Yield]
+//  get PropertyName[?Yield] ( ) { FunctionBody }
+//  set PropertyName[?Yield] ( PropertySetParameterList ) { FunctionBody }
+type MethodDefinitionNode struct {
+  node
+}
+
+// PropertySetParameterListNode  : [See 14.3]
+// FormalParameter
+type PropertySetParameterListNode struct {
+  node
+}
+
+// GeneratorMethodNode [Yield] : [See 14.4]
+//  * PropertyName[?Yield] ( StrictFormalParameters[Yield] ) { GeneratorBody }
+type GeneratorMethodNode struct {
+  node
+}
+
+// GeneratorDeclarationNode [Yield, Default] : [See 14.4]
+//  function * BindingIdentifier[?Yield] ( FormalParameters[Yield] ) { GeneratorBody }
+//  [+Default] function * ( FormalParameters[Yield] ) { GeneratorBody }
+type GeneratorDeclarationNode struct {
+  node
+}
+
+// GeneratorExpressionNode  : [See 14.4]
+//  function * BindingIdentifier[Yield]opt ( FormalParameters[Yield] ) { GeneratorBody }
+type GeneratorExpressionNode struct {
+  node
+}
+// GeneratorBodyNode  : [See 14.4]
+//  FunctionBody[Yield]
+type GeneratorBodyNode struct {
+  node
+}
+
+// YieldExpressionNode [In] : [See 14.4]
+//  yield
+//  yield [no LineTerminator here] AssignmentExpression[?In, Yield]
+//  yield [no LineTerminator here] * AssignmentExpression[?In, Yield]
+type YieldExpressionNode struct {
+  node
+}
+
+// ClassDeclarationNode [Yield, Default] : [See 14.5]
+//  class BindingIdentifier[?Yield] ClassTail[?Yield]
+//  [+Default] class ClassTail[?Yield]
+type ClassDeclarationNode struct {
+  node
+}
+
+// ClassExpressionNode [Yield] : [See 14.5]
+//  class BindingIdentifier[?Yield]opt ClassTail[?Yield]
+type ClassExpressionNode struct {
+  node
+}
+
+// ClassTailNode [Yield] : [See 14.5]
+//  ClassHeritage[?Yield]opt { ClassBody[?Yield]opt }
+type ClassTailNode struct {
+  node
+}
+
+// ClassHeritageNode [Yield] : [See 14.5]
+//  extends LeftHandSideExpression[?Yield]
+type ClassHeritageNode struct {
+  node
+}
+
+// ClassBodyNode [Yield] : [See 14.5]
+//  ClassElementList[?Yield]
+type ClassBodyNode struct {
+  node
+}
+
+// ClassElementListNode [Yield] : [See 14.5]
+//  ClassElement[?Yield]
+//  ClassElementList[?Yield] ClassElement[?Yield]
+type ClassElementListNode struct {
+  node
+}
+// ClassElementNode [Yield] : [See 14.5]
+//  MethodDefinition[?Yield]
+//  static MethodDefinition[?Yield]
+//  ;
+type ClassElementNode struct {
+  node
+}
+//
+// A.5 Scripts and Modules
+//
 
 // ScriptNode [See 15.1]
 // Script : ScriptBody*
